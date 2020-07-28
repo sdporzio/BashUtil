76baf43ab019b6b8296d949f3e075d81048bef02
Davide S. Porzio <davideporziohep@gmail.com>
2020-07-28 17:04:38 +0100

    aliases included

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
+# Setup the utils
 export SHU_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH=${SHU_DIR}/bin:${PATH}
+
+# Useful generic use aliases
+alias fav='less ~/.bash_aliases'
+alias sourcefav='source  ~/.bash_aliases'
+alias modfav='emacs -nw ~/.bash_aliases'
+alias sl='ls'
+alias ls='ls -p'
+alias root='root -l'
+alias lss='ls -lhS'
+alias emacs='emacs -nw'
